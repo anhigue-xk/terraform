f1d57b6d418728252c6e49497c43f36a8b459a3c
Anibal Higueros <ahigueros@xumak.com>
2022-08-10 11:06:11 -0600

    [add] step straight to create a server resource and connect to existed subnetwork

--- resources.tf
+++ resources.tf
@@ -1,6 +1,13 @@
-resource "google_compute_address" "our_development_network" {
+resource "google_compute_network" "our_development_network" {
     name = "devnetwork"
     auto_create_subnetwork = true
+}
+
+resource "google_compute_subnetwork" "dev-subnet" {
+    ip_cidr_range = "10.0.1.0/16"
+    name = "devsubnet"
+    network = "${google_compute_network.our_development_network.self_link}"
+    region = "us-west1"
 }
 
 resource "aws_vpc" "environment-example" {
@@ -10,4 +17,25 @@ resource "aws_vpc" "environment-example" {
     tags {
         Name = "Terraform AWS Example"
     }
+}
+
+resource "aws_subnet" "subnet1" {
+    cidr_block = "${aws_vpc.environment-example.id}"
+    vpc_id = "${aws_vpc.environment-example.id}"
+    availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = "${aws_vpc.environment-exampl.id}"
+
+    ingress = {
+        cidr_block = [
+            "${aws_vp.environment-example.cidr_block}"
+        ]
+
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+    }
+  
 }
